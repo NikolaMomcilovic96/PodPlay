5b3057601038b0774b7202f9e3ac1eff87b6acf3
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2021-12-22 17:28:04 +0100

    Finished search

--- app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/raywenderlich/podplay/ui/PodcastActivity.kt
@@ -2,6 +2,7 @@ package com.raywenderlich.podplay.ui
 
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,20 +13,19 @@ import com.raywenderlich.podplay.service.ItunesService
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import androidx.appcompat.widget.SearchView
+import com.raywenderlich.podplay.databinding.ActivityPodcastBinding
 
 class PodcastActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPodcastBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_podcast)
 
-        val itunesService = ItunesService.instance
-        val itunesRepo = ItunesRepo(itunesService)
+        binding = ActivityPodcastBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        GlobalScope.launch {
-            val results = itunesRepo.searchByTerm("Android Developer")
-            Log.i("RES", "Results = ${results.body()}")
-        }
+        setupToolbar()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -37,5 +37,32 @@ class PodcastActivity : AppCompatActivity() {
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
 
         return true
+    }
+
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        GlobalScope.launch {
+            val results = itunesRepo.searchByTerm(term)
+            Log.i("RES", "Results = ${results.body()}")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
     }
 }
